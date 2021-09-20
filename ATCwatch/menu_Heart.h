53cc54854ffb2387a6dd429fd3ba25717d9f7cf4
androidonis <81442422+androidonis@users.noreply.github.com>
2021-09-20 16:24:59 +0200

    Update menu_Heart.h
    show min, max, avg.

--- ATCwatch/menu_Heart.h
+++ ATCwatch/menu_Heart.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020 Aaron Christophel
- *
+ * modifications (c) 2021 Andreas Loew
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -28,17 +28,29 @@ class HeartScreen : public Screen
 
       label = lv_label_create(lv_scr_act(), NULL);
       lv_label_set_text(label, "Heartrate");
-      lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+      lv_obj_align(label, NULL, LV_ALIGN_IN_TOP_LEFT, 60, 0);
 
       set_gray_screen_style(&lv_font_roboto_28);
       
       label_hr = lv_label_create(lv_scr_act(), NULL);
-      lv_label_set_text(label_hr, "HR:");
-      lv_obj_align(label_hr, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 50);
+      lv_label_set_text(label_hr, "now");
+      lv_obj_align(label_hr, NULL, LV_ALIGN_IN_TOP_LEFT, 88, 40);
+
+      label_hrmi = lv_label_create(lv_scr_act(), NULL);
+      lv_label_set_text(label_hrmi, "min");
+      lv_obj_align(label_hrmi, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 40);
+
+      label_hrma = lv_label_create(lv_scr_act(), NULL);
+      lv_label_set_text(label_hrma, "max");
+      lv_obj_align(label_hrma, NULL, LV_ALIGN_IN_TOP_LEFT, 175, 40);
 
       label_hr_last = lv_label_create(lv_scr_act(), NULL);
-      lv_label_set_text(label_hr_last, "Last HR:");
-      lv_obj_align(label_hr_last, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 80);
+      lv_label_set_text(label_hr_last, "Last:");
+      lv_obj_align(label_hr_last, NULL, LV_ALIGN_IN_TOP_LEFT, 5, 80);
+
+      label_hrav = lv_label_create(lv_scr_act(), NULL);
+      lv_label_set_text(label_hrav, "Avg:");
+      lv_obj_align(label_hrav, NULL, LV_ALIGN_IN_TOP_LEFT, 120, 80);
 
       lv_obj_t * img1 = lv_img_create(lv_scr_act(), NULL);
       lv_img_set_src(img1, &IsymbolHeart);
@@ -53,16 +65,30 @@ class HeartScreen : public Screen
         case 255:
           break;
         case 254:
-          lv_label_set_text_fmt(label_hr, "HR: No Touch");
+          lv_label_set_text_fmt(label_hr, "NoT");
           break;
         case 253:
-          lv_label_set_text_fmt(label_hr, "HR: Please Wait");
+          lv_label_set_text_fmt(label_hr, "Wait");
           break;
         default:
-          lv_label_set_text_fmt(label_hr, "HR: %i", hr);
+          lv_label_set_text_fmt(label_hr, "%i", hr);
           break;
       }
-      lv_label_set_text_fmt(label_hr_last, "Last HR: %i", get_last_heartrate());
+      byte hrma = 1;
+      byte hrmi = 250;
+      uint32_t hrav = 0;
+      int16_t hrcnt=0;
+      for (int lpct = 0; lpct < 500; lpct++) {
+        if  (get_hearthistory(lpct)>0) {
+          hrav+=get_hearthistory(lpct); hrcnt++; 
+          if (get_hearthistory(lpct)>hrma) hrma=get_hearthistory(lpct); 
+          if (get_hearthistory(lpct)<hrmi) hrmi=get_hearthistory(lpct);   
+          }
+      }
+      lv_label_set_text_fmt(label_hrmi, "%i", hrmi);
+      lv_label_set_text_fmt(label_hrma, "%i", hrma);
+      lv_label_set_text_fmt(label_hrav, "Avg:%i", (hrav+(hrcnt/2))/hrcnt);
+      lv_label_set_text_fmt(label_hr_last, "Last:%i", get_last_heartrate());
     }
 
     virtual void post()
@@ -81,7 +107,7 @@ class HeartScreen : public Screen
     }
 
   private:
-    lv_obj_t *label, *label_hr, *label_hr_last;
+    lv_obj_t *label, *label_hr, *label_hrmi, *label_hrma, *label_hrav, *label_hr_last;
 
 };
 
