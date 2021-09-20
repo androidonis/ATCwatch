b0ad4aabc3ad8d3fc2ebb60bcaa27c5c18cdd274
androidonis <81442422+androidonis@users.noreply.github.com>
2021-09-20 16:19:59 +0200

    Update backlight.cpp
    Low backlight (during night hours) with PWM use

--- ATCwatch/backlight.cpp
+++ ATCwatch/backlight.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020 Aaron Christophel
- *
+ * modifications (c) 2021 Andreas Loew
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -11,6 +11,8 @@
 int backlight_brightness = 0;
 int min_backlight_brightness = 1;
 int max_backlight_brightness = 7;
+uint8_t low_brightness = 224;
+bool use_pwm = true;
 
 void init_backlight() {
   pinMode(LCD_BACKLIGHT_LOW, OUTPUT);
@@ -19,6 +21,16 @@ void init_backlight() {
 
   set_backlight(0);
 }
+
+void set_bright(uint8_t lowbright)
+{
+low_brightness = lowbright;   
+  }
+
+void disp_pwm(bool state)
+{
+use_pwm = state;   
+  }
 
 void set_backlight() {
   set_backlight(backlight_brightness);
@@ -29,45 +41,45 @@ int get_backlight() {
 }
 
 void set_backlight(int brightness) {
-  if (brightness)backlight_brightness = brightness;
+  if ((brightness>0)&&(brightness<7)) backlight_brightness = brightness;
   switch (brightness) {
     case 0:
-      digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 255); else digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
       digitalWrite(LCD_BACKLIGHT_MID, HIGH);
       digitalWrite(LCD_BACKLIGHT_HIGH, HIGH);
       break;
     case 1:
-      digitalWrite(LCD_BACKLIGHT_LOW, LOW);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, low_brightness); else digitalWrite(LCD_BACKLIGHT_LOW, LOW);
       digitalWrite(LCD_BACKLIGHT_MID, HIGH);
       digitalWrite(LCD_BACKLIGHT_HIGH, HIGH);
       break;
     case 2:
-      digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 255); else digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
       digitalWrite(LCD_BACKLIGHT_MID, LOW);
       digitalWrite(LCD_BACKLIGHT_HIGH, HIGH);
       break;
     case 3:
-      digitalWrite(LCD_BACKLIGHT_LOW, LOW);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 0); else digitalWrite(LCD_BACKLIGHT_LOW, LOW);
       digitalWrite(LCD_BACKLIGHT_MID, LOW);
       digitalWrite(LCD_BACKLIGHT_HIGH, HIGH);
       break;
     case 4:
-      digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 255); else digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
       digitalWrite(LCD_BACKLIGHT_MID, HIGH);
       digitalWrite(LCD_BACKLIGHT_HIGH, LOW);
       break;
     case 5:
-      digitalWrite(LCD_BACKLIGHT_LOW, LOW);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 0); else digitalWrite(LCD_BACKLIGHT_LOW, LOW);
       digitalWrite(LCD_BACKLIGHT_MID, HIGH);
       digitalWrite(LCD_BACKLIGHT_HIGH, LOW);
       break;
     case 6:
-      digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 255); else digitalWrite(LCD_BACKLIGHT_LOW, HIGH);
       digitalWrite(LCD_BACKLIGHT_MID, LOW);
       digitalWrite(LCD_BACKLIGHT_HIGH, LOW);
       break;
     case 7:
-      digitalWrite(LCD_BACKLIGHT_LOW, LOW);
+      if (use_pwm) analogWrite(LCD_BACKLIGHT_LOW, 0); else digitalWrite(LCD_BACKLIGHT_LOW, LOW);
       digitalWrite(LCD_BACKLIGHT_MID, LOW);
       digitalWrite(LCD_BACKLIGHT_HIGH, LOW);
       break;
