b827d5a7933040de5cb73bb81a94685dc00fed64
androidonis <81442422+androidonis@users.noreply.github.com>
2022-03-19 19:19:46 +0100

    calculation watch angle
    Added arcsin function to calculate angle of watch

--- ATCwatch/accl.cpp
+++ ATCwatch/accl.cpp
@@ -15,6 +15,11 @@
 #include "sleep.h"
 
 struct accl_data_struct accl_data;
+struct angle_data angle;
+// calibration of sensor for flat table
+const int  acc_calib_x = 0;
+const int  acc_calib_y = 30;
+const int  acc_calib_z = 8;
 
 void init_accl() {
   pinMode(BMA421_INT, INPUT);
@@ -107,13 +112,58 @@ void update_accl_data() {
   user_i2c_read(0x18, 0x12, (uint8_t *)&accl_data.x, 6);
 #ifdef SWITCH_X_Y // pinetime has 90° rotated Accl
   short tempX = accl_data.x;
-  accl_data.x = accl_data.y;
+  accl_data.x = accl_data.y
   accl_data.y = tempX;
 #endif
-  accl_data.x = (accl_data.x / 0x10);
-  accl_data.y = (accl_data.y / 0x10);
-  accl_data.z = (accl_data.z / 0x10);
+  accl_data.x = (accl_data.x / 0x10) + acc_calib_x;
+  accl_data.y = (accl_data.y / 0x10) + acc_calib_y;
+  accl_data.z = (accl_data.z / 0x10) + acc_calib_z;
 }
+
+
+uint16_t arcsin( uint32_t avalue) // input 00000-100000 output 0000-9000
+   { uint32_t svalue, sum1, z1, n1, add;
+    if (avalue<96350){
+     svalue = avalue/100; z1=1; n1=1; sum1=avalue;
+     for (uint8_t iter = 3; iter <119; iter+=2) {
+       z1 = z1 * (iter-2); n1 = n1 * (iter-1);
+       if((z1>65535)||(n1>65535)){z1=z1/64; n1=n1/64;}
+       avalue = ((avalue/100) * svalue * svalue)/10000;
+       add = (((avalue/iter)*z1+(n1>>1))/(n1));
+       sum1 = sum1 + add;
+       if (add<1) iter = 119; // stop if add gets too small
+       }
+     sum1 = sum1*10000/174533;
+    }
+    else {
+     sum1 = 9000; svalue = 99990; z1 = 16; 
+     for (uint8_t iter = 89; iter > 74; iter--){
+       if (avalue < svalue) sum1 = iter*100;
+       z1 = z1 +30;
+       svalue = svalue - z1;
+        }     
+    }
+    return sum1;   
+    }
+
+
+angle_data getangle(){
+  uint32_t acvalue;
+  bool sign;
+
+    // update_accl_data();
+  acvalue = (abs(accl_data.x)*100000)/512; 
+  if (accl_data.x < 0) angle.x = (0-arcsin(acvalue)-50)/100; else angle.x = (arcsin(acvalue)+50)/100; 
+  
+  acvalue = (abs(accl_data.y)*100000)/512; 
+  if (accl_data.y < 0) angle.y = (0-arcsin(acvalue)-50)/100; else angle.y = (arcsin(acvalue)+50)/100; 
+  
+  acvalue = (abs(accl_data.z)*100000)/512; 
+  if (accl_data.z < 0) angle.z = (0-arcsin(acvalue)-50)/100; else angle.z = (arcsin(acvalue)+50)/100;  
+
+  return angle;
+   
+  }
 
 void accl_write_reg(uint8_t reg, uint8_t data) {
   user_i2c_write(0x18, reg, &data, 1);
