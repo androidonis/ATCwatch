74f361a58a04a40972a1fa13f9ac101fbd98daef
androidonis <81442422+androidonis@users.noreply.github.com>
2022-03-19 19:39:41 +0100

    select measurement interval

--- ATCwatch/heartrate.cpp
+++ ATCwatch/heartrate.cpp
@@ -22,7 +22,8 @@ bool has_good_heartrate = false;
 int hr_answers;
 bool disabled_hr_allready = false;
 uint8_t heartrates[601] = {0}; //history of rates
-int nextminute = 99; 
+int nextminute = 99;
+int hrevery = 5; 
 byte hr;
 
 void init_hrs3300() {
@@ -77,6 +78,14 @@ byte get_hearthistory(uint16_t history) {
   return heartrates[history];
 }
 
+void sethrevery(int interv){
+  hrevery = interv;
+  }
+
+int get_hrint(){
+  return hrevery;
+  }
+
 void check_timed_heartrate(int minutes) {
   if (timed_heart_rates) {
     if (minutes == nextminute || nextminute == 99 ) {
@@ -97,7 +106,7 @@ void check_timed_heartrate(int minutes) {
         }
       } else {
         end_hrs3300();
-        nextminute = minutes + 1; if (nextminute > 59) nextminute = nextminute - 60;
+        nextminute = minutes + hrevery; if (nextminute > 59) nextminute = nextminute - 60;
         for (int16_t lp=599; lp>0; lp--) heartrates[lp] = heartrates[lp-1]; //shift up history
         heartrates[0]=hr; // just take last measurement, maybe median filter the 5 measurements.
         heartrates[600]=nextminute;
